e629c055d1cb00f26d8ff16a0ae70475cbe93972
aartikansal <aartilumb@gmail.com>
2020-02-17 12:02:27 -0500

    basic string utils done

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -1,5 +1,10 @@
 package com.zipcodewilmington.assessment1.part1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -9,7 +14,11 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        str = str.substring(0, 1).toUpperCase() + str.substring(1);
+
+        System.out.println(" The camelCase string  is -->" + str);
+
+        return str;
     }
 
     /**
@@ -17,7 +26,17 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+
+
+        System.out.println("Enter string to reverse:");
+        String strNew = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            strNew = strNew + str.charAt(i);
+        }
+        System.out.println(" The reversed string  is -->" + strNew);
+        return strNew;
+
+
     }
 
     /**
@@ -25,9 +44,21 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
-    }
 
+        System.out.println("Enter string to reverse:");
+        String strNew1 = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            strNew1 = strNew1 + str.charAt(i);
+
+
+        }
+        strNew1 = strNew1.substring(0, 1).toUpperCase() + strNew1.substring(1);
+
+        System.out.println(" The reversed string  is -->" + strNew1);
+
+
+        return strNew1;
+    }
 
     /**
      * @param str a string input from user
@@ -37,11 +68,22 @@ public class BasicStringUtils {
         return null;
     }
 
+
     /**
      * @param str a string input from user
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+
+
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = Character.isUpperCase(chars[i])
+                    ? Character.toLowerCase(chars[i])
+                    : Character.toUpperCase(chars[i]);
+        }
+        return new String(chars);
+
+
     }
 }
